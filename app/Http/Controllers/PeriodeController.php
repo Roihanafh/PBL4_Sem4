8d797eecfe9945c4528a8d951e25b642e811b884
Roihan Al Fahmi <roihanalfahmi123@gmail.com>
2025-05-17 17:03:45 +0700

    add crud periode

--- app/Http/Controllers/PeriodeController.php
+++ app/Http/Controllers/PeriodeController.php
@@ -37,7 +37,7 @@ class PeriodeController extends Controller
             ->addIndexColumn()
             ->addColumn('aksi', function ($prd) {
                 $btn  = '<div class="btn-group" role="group">';
-                $btn .= '<button onclick="modalAction(\''.url('/periode/' . $prd->periode_id . '/detail_ajax').'\')" class="btn btn-primary btn-sm" style="margin-right: 5px;" title="Detail Data">';
+                $btn .= '<button onclick="modalAction(\''.url('/periode/' . $prd->periode_id . '/show_ajax').'\')" class="btn btn-primary btn-sm" style="margin-right: 5px;" title="Detail Data">';
                 $btn .= '<i class="fas fa-info-circle"></i></button>';
                 $btn .= '<button onclick="modalAction(\''.url('/periode/' . $prd->periode_id . '/edit_ajax').'\')" class="btn btn-warning btn-sm" style="margin-right: 5px;" title="Edit Data">';
                 $btn .= '<i class="fas fa-edit"></i></button>';
@@ -49,5 +49,131 @@ class PeriodeController extends Controller
             })
             ->rawColumns(['aksi'])
             ->make(true);
+    }
+
+    public function create_ajax()
+    {
+        return view('periode.create_ajax');
+    }
+
+
+
+    public function store_ajax(Request $request)
+    {
+        $validatedPeriode = $request->validate([
+            'periode' => 'required',
+            'keterangan' => 'required',
+        ]);
+
+        PeriodeMagangModel::create([
+            'periode' => $validatedPeriode['periode'],
+            'keterangan' => $validatedPeriode['keterangan'],
+        ]);
+
+       return response()->json([
+        'status' => true,
+        'message' => 'Data periode berhasil disimpan'
+        ]);
+    }
+
+    public function confirm_ajax($periode_id)
+    {
+        $periode = PeriodeMagangModel::select()->where('periode_id', $periode_id)->first();
+
+        return view('periode.confirm_ajax', compact('periode'));
+    }
+
+    public function delete_ajax(Request $request, $periode_id)
+    {
+        try {
+            $periode = PeriodeMagangModel::where('periode_id', $periode_id)->first();
+
+            if (!$periode) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data periode tidak ditemukan.'
+                ], 404);
+            }
+
+            // Hapus periode
+            $periode->delete();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Data periode berhasil dihapus.'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Terjadi kesalahan saat menghapus data: ' . $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function show_ajax(String $periode_id)
+    {
+        $periode = PeriodeMagangModel::select('periode_id', 'periode', 'keterangan')->where('periode_id', $periode_id)->first();
+
+        if (!$periode) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Data periode dengan id ' . $periode_id . ' tidak ditemukan.'
+            ], 404);
+        }
+
+        return view('periode.show_ajax', [
+            'periode' => $periode
+        ]);
+    }
+
+    public function edit_ajax($periode_id)
+    {
+        $periode = PeriodeMagangModel::select()->where('periode_id', $periode_id)->first();
+
+        return view('periode.edit_ajax', compact('periode'));
+    }
+
+    public function update_ajax(Request $request, $periode_id)
+    {
+        $periode = PeriodeMagangModel::select()->find($periode_id);
+
+        if (!$periode) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Data mahasiswa tidak ditemukan.'
+            ]);
+        }
+
+        // Validasi input
+        $validator = Validator::make($request->all(), [
+            'periode'  => 'required|max:20',
+            'keterangan' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validasi gagal, periksa input anda.',
+                'msgField' => $validator->errors()
+            ]);
+        }
+
+        try {
+            // Update data periode
+            $periode->periode = $request->periode;
+            $periode->keterangan = $request->keterangan;
+            $periode->save();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Data periode berhasil diperbarui.'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Terjadi kesalahan saat menyimpan data.',
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 }
