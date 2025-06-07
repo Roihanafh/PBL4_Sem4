7b0d3b9a8c983b9a1ba1cbd85b0cddce252621dc
salsabilamhda <salsabilamahda217@gmail.com>
2025-06-07 12:24:29 +0700

    update vie profile at dashboard admin

--- resources/views/admin/welcome_admin.blade.php
+++ resources/views/admin/welcome_admin.blade.php
@@ -169,6 +169,12 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="static" data-keyboard="false">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content"></div>
+      </div>
+    </div>
 @endsection
 
 @push('js')
@@ -276,5 +282,20 @@
                 }
             });
         });
+
+        $(function () {
+    // Setup CSRF token untuk semua AJAX
+        $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+        });
+    });
+
+    function modalAction(url = ''){
+            $('#myModal .modal-content').load(url,function(){
+                $('#myModal').modal('show');
+            });
+        }
     </script>
 @endpush
