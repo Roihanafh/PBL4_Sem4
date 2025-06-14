68239861b65f605ce21e4648d6acbd99f14ea81f
salsabilamhda <salsabilamahda217@gmail.com>
2025-06-14 19:10:11 +0700

    update mhs & dosen seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -26,17 +26,91 @@ class UserSeeder extends Seeder
                 'level_id' => 2, // Dosen
             ],
             [
+                'username' => 'dosen2',
+                'password' => Hash::make('12345'),
+                'level_id' => 2, // Dosen
+            ],
+             [
+                'username' => 'dosen3',
+                'password' => Hash::make('12345'),
+                'level_id' => 2, // Dosen
+            ],
+             [
+                'username' => 'dosen4',
+                'password' => Hash::make('12345'),
+                'level_id' => 2, // Dosen
+            ],
+             [
+                'username' => 'dosen5',
+                'password' => Hash::make('12345'),
+                'level_id' => 2, // Dosen
+            ],
+             [
+                'username' => 'dosen6',
+                'password' => Hash::make('12345'),
+                'level_id' => 2, // Dosen
+            ],
+             [
+                'username' => 'dosen7',
+                'password' => Hash::make('12345'),
+                'level_id' => 2, // Dosen
+            ],
+             [
+                'username' => 'dosen8',
+                'password' => Hash::make('12345'),
+                'level_id' => 2, // Dosen
+            ],
+             [
+                'username' => 'dosen9',
+                'password' => Hash::make('12345'),
+                'level_id' => 2, // Dosen
+            ],
+             [
+                'username' => 'dosen10',
+                'password' => Hash::make('12345'),
+                'level_id' => 2, // Dosen
+            ],
+            [
                 'username' => 'mahasiswa',
                 'password' => Hash::make('mhs'),
                 'level_id' => 3, // Mahasiswa
             ],
             [
-                'username' => 'mahasiswaa',
+                'username' => 'mhs2',
                 'password' => Hash::make('mhs'),
                 'level_id' => 3, // Mahasiswa
             ],
             [
-                'username' => 'mahasiswaaa',
+                'username' => 'mhs3',
+                'password' => Hash::make('mhs'),
+                'level_id' => 3, // Mahasiswa
+            ],
+            [
+                'username' => 'mhs4',
+                'password' => Hash::make('mhs'),
+                'level_id' => 3, // Mahasiswa
+            ],[
+                'username' => 'mhs5',
+                'password' => Hash::make('mhs'),
+                'level_id' => 3, // Mahasiswa
+            ],[
+                'username' => 'mhs6',
+                'password' => Hash::make('mhs'),
+                'level_id' => 3, // Mahasiswa
+            ],[
+                'username' => 'mhs7',
+                'password' => Hash::make('mhs'),
+                'level_id' => 3, // Mahasiswa
+            ],[
+                'username' => 'mhs8',
+                'password' => Hash::make('mhs'),
+                'level_id' => 3, // Mahasiswa
+            ],[
+                'username' => 'mhs9',
+                'password' => Hash::make('mhs'),
+                'level_id' => 3, // Mahasiswa
+            ],[
+                'username' => 'mhs10',
                 'password' => Hash::make('mhs'),
                 'level_id' => 3, // Mahasiswa
             ],
